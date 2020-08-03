4968de9cf753aac7d7fb57055290606f9c34b08f
linjunfu <linjunfu@youxin.com>
2020-08-03 13:12:53 +0800

    feat: 04-react-router

--- react/03-hooks__react-redux/lesson3/src/App.js
+++ react/03-hooks__react-redux/lesson3/src/App.js
@@ -1,13 +1,13 @@
 import React, {useState} from "react";
 // import HooksPage from "./pages/HooksPage";
-// import ReactReduxPage from "./pages/ReactReduxPage";
+import ReactReduxPage from "./pages/ReactReduxPage";
 import ReduxHooksPage from "./pages/ReduxHooksPage";
 
 export default function App(props) {
   return (
     <div>
       {/* <HooksPage /> */}
-      {/* <ReactReduxPage /> */}
+      <ReactReduxPage />
       <ReduxHooksPage />
     </div>
   );
