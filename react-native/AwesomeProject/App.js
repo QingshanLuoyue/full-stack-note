0a48b52ae633b970c07c90b6b434e11143319c58
linjunfu <linjunfu@youxin.com>
2020-10-22 00:08:55 +0800

    feat: webview example

--- react-native/AwesomeProject/App.js
+++ react-native/AwesomeProject/App.js
@@ -5,95 +5,43 @@
  * @format
  * @flow strict-local
  */
-// 暗号：明确状态归属，合理切分组件
+// 暗号：技术为生活服务
 import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  View,
-  Text,
-  TouchableOpacity,
-  StyleSheet,
-  Switch,
-  Dimensions,
-} from 'react-native';
+import WebView from 'react-native-webview';
+import {View, Text, Alert} from 'react-native';
 
-// 获取屏幕宽高
-const {width, height} = Dimensions.get('window');
-const cellWidth = width * 0.3;
+const uri = 'https://m.mtime.cn/#!/onlineticket/614532488/';
+
+const injectJsContent = (window, document) => {
+  let submitBtn;
+  function waitForSubmitBtnRender() {
+    submitBtn = document.getElementById('submitBtn');
+    if (!submitBtn) {
+      setTimeout(waitForSubmitBtnRender, 2000);
+    } else {
+      submitBtn.onclick = () => {
+        const seats = [];
+        document.querySelectorAll('.seat_selected').forEach((node) => {
+          seats.push(node.getAttribute('name'));
+        });
+        window.ReactNativeWebView.postMessage(seats.join(','));
+      };
+    }
+  }
+  waitForSubmitBtnRender();
+};
 
 function App() {
-  // 是否单选
-  const [isSingle, setIsSingle] = React.useState(false);
-  // 是否选中
-  const [isSelectedIdx, setIsSelectedIdx] = React.useState(0);
   return (
-    <View style={styles.container}>
-      <View style={styles.innerContainer}>
-        <Text>单选</Text>
-        <Switch
-          style={{marginLeft: 15}}
-          value={isSingle}
-          onValueChange={setIsSingle}></Switch>
-      </View>
-      <View style={styles.innerContainer}>
-        {isSingle
-          ? [...new Array(9)].map((_, i) => {
-              return (
-                <TouchableOpacity
-                  key={i}
-                  style={[
-                    styles.cell,
-                    isSelectedIdx === i && {backgroundColor: 'blue'},
-                  ]}
-                  onPress={() => {
-                    // 选中，则变色
-                    setIsSelectedIdx(i);
-                  }}>
-                  <Text>{i + 1}</Text>
-                </TouchableOpacity>
-              );
-            })
-          : [...new Array(9)].map((_, i) => {
-              return <Cell></Cell>;
-            })}
-      </View>
-    </View>
-  );
-}
-
-function Cell() {
-  const [selected, setSelected] = React.useState(false);
-  // 选择变色
-  return (
-    <TouchableOpacity
-      onPress={() => setSelected(!selected)}
-      style={[
-        styles.cell,
-        selected && {backgroundColor: 'blue'},
-      ]}></TouchableOpacity>
+    <WebView
+      source={{uri}}
+      injectedJavaScript={`(${injectJsContent.toString()}}(window, document)`}
+      onMessage={(e) => {
+        Alert.alert('您选中的座位是:' + e.nativeEvent.data);
+      }}
+    />
   );
 }
 
 export default App;
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-  },
-  innerContainer: {
-    marginTop: 50,
-    justifyContent: 'center',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-  },
-  cell: {
-    width: cellWidth,
-    height: cellWidth,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    borderColor: 'gray',
-    borderWidth: 1,
-  },
-});
